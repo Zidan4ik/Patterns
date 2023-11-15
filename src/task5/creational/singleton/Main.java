bc4423667ef0ba2ba456dada516c250bdbbf3445
Zidan4ik <roman.pravnyk.tr.2021@lpnu.ua>
2023-11-15 12:15:42 +0200

    update singleton

---
+++ src/task5/creational/singleton/Main.java
@@ -0,0 +1,10 @@
+package task5.creational.singleton;
+
+public class Main {
+    public static void main(String[] args) {
+        Psychologist psy = Psychologist.getInstance();
+        psy.workTime("Denys",2);
+        psy.workTime("Mark",1);
+//        Psychologist psy2 = new Psychologist();
+    }
+}
--- src/task5/creational/singleton/Psychologist.java
+++ src/task5/creational/singleton/Psychologist.java
@@ -2,24 +2,16 @@ package task5.creational.singleton;
 
 public class Psychologist{
     private static Psychologist psychologist;
-
+    private Psychologist() {
+    }
     public static Psychologist getInstance(){
         if(psychologist == null){
             psychologist = new Psychologist();
         }
         return psychologist;
     }
-
-    private Psychologist() {
-        System.out.println("психолог тільки що був назначний");
-    }
     public void workTime(String namePerson,double time){
         System.out.println("Психолог уже спілкується "+time+" години з "+ namePerson);
     }
 
-    public static void main(String[] args) {
-        Psychologist.getInstance().workTime("Denys",5);
-        Psychologist.getInstance().workTime("Denys",6);
-        Psychologist.getInstance().workTime("Roman",2);
-    }
 }
