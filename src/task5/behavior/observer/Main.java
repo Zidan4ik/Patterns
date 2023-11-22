99ddbd540453548327423d44d40f76c1a2d2b68d
Zidan4ik <roman.pravnyk.tr.2021@lpnu.ua>
2023-11-22 21:36:09 +0200

    update patterns

--- src/task5/behavior/observer/Main.java
+++ src/task5/behavior/observer/Main.java
@@ -9,6 +9,8 @@ public class Main {
 
         tracker.add(roma);
         tracker.add(doctor);
+
+        System.out.println("Патерн Observer");
         System.out.println("Повідомлення перше:");
         tracker.notifyPersons();
 
--- src/task5/behavior/state/Main.java
+++ src/task5/behavior/state/Main.java
@@ -1,9 +1,13 @@
 package task5.behavior.state;
 
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
         TrafficLight trafficLight = new TrafficLight(new Green());
-        for(int i=0;i<20;i++){
+
+        System.out.println("Патерн State");
+        for(int i=0;i<25;i++){
             trafficLight.blinkingColor();
         }
     }
